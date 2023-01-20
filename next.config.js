f19e1ecdcde67679a1967e2dc2fdb8a538f6fdf3
zattadigital <contato.zattadigital@gmail.com>
2023-01-20 14:10:30 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    env: {
-        emailAppPass: "ktyqzhvcxrvamesu",
-        emailSendFrom: "zattaprojects@gmail.com",
-        emailSendTo: "contato.zattadigital@gmail.com",
-    },
 };
 
 module.exports = nextConfig;
