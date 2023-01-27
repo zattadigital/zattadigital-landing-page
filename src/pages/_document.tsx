d40cb50cae87368c39e8bd5757be4ebf00307e3c
zattadigital <contato.zattadigital@gmail.com>
2023-01-27 18:26:33 -0300

    gtm config test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,27 +4,11 @@ import Script from "next/script";
 export default function Document() {
     return (
         <Html lang="en">
-            <Head>
-                {/* <Script id="google-analytics" strategy="afterInteractive">
-                    {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-KN3XQ7C');          
-        `}
-                </Script> */}
-            </Head>
+            <Head></Head>
             <body id="body">
-                {/* <noscript>
-                    <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-KN3XQ7C"
-                        height="0"
-                        width="0"
-                        style={{display:'none', visibility:'hidden'}}
-                    ></iframe>
-                </noscript> */}
-
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`gtag('event', 'conversion', {'send_to': 'AW-11078796620/22ccCKqB04kYEMyK5KIp'});`}
+                </Script>
                 <Main />
                 <NextScript />
             </body>
