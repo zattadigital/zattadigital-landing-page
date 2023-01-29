9166a522b2786b699f40982ce7204f305b91a125
zattadigital <contato.zattadigital@gmail.com>
2023-01-29 11:31:46 -0300

    gtm config test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,23 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
+import { useEffect } from "react";
 
 export default function Document() {
     return (
         <Html lang="en">
-            <Head></Head>
+            <Head>
+                <Script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=AW-11078796620"
+                ></Script>
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11078796620');`}
+                </Script>
+            </Head>
             <body id="body">
                 <Script id="google-analytics" strategy="afterInteractive">
                     {`gtag('event', 'conversion', {'send_to': 'AW-11078796620/22ccCKqB04kYEMyK5KIp'});`}
